131bf5f2305d450802385b3cd1960f697cc23878
chenf <chenf@surest.cn>
2023-02-28 18:05:05 +0800

    LOGO 替换

--- lib/res/r.dart
+++ lib/res/r.dart
@@ -1,6 +1,5 @@
 class R {
-  static const String assetsImagesLoginBackground= 'assets/images/3.0x/bg_login.png';
-  static const String assetsImagesApplication= 'assets/images/3.0x/application_icon.png';
+  static const String assetsImagesApplication= 'assets/icons/icon.png';
 
   ///----------------------------lottie---------------------------
   static const String assetsLottieLoading = 'assets/lottie/lottie_common_load.json';
